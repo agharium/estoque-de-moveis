54c95430aaf3797defadc23f29251b74ea6296be
João Miguel <oaojmiguel@outlook.com>
2017-11-03 21:04:34 -0200

    Modificação da Classe Estoque
    Adição dos gets e sets

--- app/models/Estoque.php
+++ app/models/Estoque.php
@@ -9,5 +9,53 @@
         private $data;
         private $Produto;
         private $Usuario;
+
+        public function setCodigo($codigo){
+            $this->codigo = $codigo;
+        }
+
+        public function getCodigo(){
+            return $this->codigo;
+        }
+
+        public function setQuantidade($quantidade){
+            $this->quantidade = $quantidade;
+        }
+
+        public function getQuantidade(){
+            return $this->quantidade;
+        }
+
+        public function setComentario($comentario){
+            $this->comentario = $comentario;
+        }
+
+        public function getComentario(){
+            return $this->comentario;
+        }
+
+        public function setData($data){
+            $this->data = $data;
+        }
+
+        public function getData(){
+            return $this->data;
+        }
+
+        public function setProduto($Produto){
+            $this->Produto = $Produto;
+        }
+
+        public function getProduto(){
+            return $this->Produto;
+        }
+
+        public function setUsuario($Usuario){
+            $this->Usuario = $Usuario;
+        }
+
+        public function getUsuario(){
+            return $this->Usuario;
+        }
     }
 ?>
