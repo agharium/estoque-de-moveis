f59bd599a5582e78c28ce7556d082048658a20c5
José Paulo Oliveira Filho <oliveira.filho237@gmail.com>
2017-11-11 08:04:01 -0200

    cool way to select row on table: done

--- app/views/home/dashboard/index.php
+++ app/views/home/dashboard/index.php
@@ -2,7 +2,6 @@
 	$title = "Estoque de Móveis - Painel de Controle";
 	$footer = false;
 	require(HOME_PATH . "head.php");
-	echo $_COOKIE["produto"];
 ?>
 	<div class="container">
 		<h2 class="text-center"> PRODUTOS </h2>
@@ -10,6 +9,7 @@
 			<table id="table">
 				<thead>
 					<tr>
+						<th></th>
 						<th>ID</th>
 						<th>Nome</th>
 						<th>Preço</th>
@@ -18,16 +18,25 @@
 				</thead>
 				<tbody>
 					<tr>
+						<td><input type="radio" name="select"></td>
 						<td>1</td>
 						<td>Produto 1</td>
 						<td>R$ 100</td>
 						<td>10</td>
 					</tr>
 					<tr>
+						<td><input type="radio" name="select"></td>
 						<td>2</td>
 						<td>Produto 2</td>
 						<td>R$ 200</td>
 						<td>20</td>
+					</tr>
+					<tr>
+						<td><input type="radio" name="select"></td>
+						<td>3</td>
+						<td>Produto 3</td>
+						<td>R$ 300</td>
+						<td>30</td>
 					</tr>
 				</tbody>
 			</table>
@@ -35,47 +44,55 @@
 	</div>
 	
 	<script>
-		/*function tabelaHandler() {
-		    var table = document.getElementById("table");
-		    var rows = table.getElementsByTagName("tr");
-		    for (i = 0; i < rows.length; i++) {
-		        var currentRow = table.rows[i];
-		        var createClickHandler = 
-		            function(row) 
-		            {
-		                return function() { 
-                            var cell = row.getElementsByTagName("td")[0];
-                            var id = cell.innerHTML;
-                            document.cookie = "produto=" + id;
-                     	};
-		            };
-		        currentRow.onclick = createClickHandler(currentRow);
-		    }
-		}
-		window.onload = tabelaHandler();*/
+		var hover = true;
 
 		$(document).ready(function(){
-		    $("table tr").hover(function() {
-		    	console.log($(this).closest('tr').find('td:first-child').html());
-		        $(this).toggleClass("tr-hover");
+			$("tbody tr").hover(function() {
+				if (hover){
+					$(this).toggleClass("tr-hover");
+				}
+		    });
+
+			$("tbody tr").click(function() {
+				var selectedRadio = $("table").find('input:radio:checked');
+				var radio = $(":first-child > :first-child", this);
+
+				if(!radio.is(':checked') && selectedRadio.length == 0){
+					document.cookie = "produto=" + $(this).find('td:nth-child(2)').html();
+					console.log("Cookie adicionado: " + document.cookie);
+			    	
+			    	hover = false;
+			        
+			        radio.prop("checked", true);
+				} else if (radio.is(':checked')) {
+					document.cookie = "produto=" + 0;
+					console.log("Cookie removido: " + document.cookie);
+
+					hover = true;
+
+			        radio.prop("checked", false);
+				}
 		    });
 		});
 	</script>
 
 	<style type="text/css">
-		tbody tr {
-			cursor: pointer;
-		}
-
-
-		/*tbody tr:hover*/
 		.tr-hover {
 			background: #2ecc71;
 			font-weight: 500;
+			cursor: pointer;
 		}
 
 		th, td {
 			text-align: center;
+		}
+
+		td:first-child, th:first-child {
+			display: none;
+		}
+
+		input[name="selected"] {
+		  	pointer-events: none;
 		}
 	</style>
 
