326a5c498c9a83e11e83dbae21e901796566465f
José Paulo Oliveira Filho <oliveira.filho237@gmail.com>
2017-11-11 11:01:00 -0200

    off-screen cdn added

--- app/views/home/head.php
+++ app/views/home/head.php
@@ -6,6 +6,10 @@
 	<link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
 	<link rel="stylesheet" href="//rawgit.com/agharium/estoque-de-moveis/master/css/custom-css.css">
 	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+	<?php if ($title == "Estoque de Móveis - Painel de Controle"){
+		echo
+	"<link rel='stylesheet' href='https://rawgit.com/agharium/estoque-de-moveis/master/css/offscreen-menu.css'>";
+	} ?>
 
 	<title> <?php echo $title ?> </title>
 </head>
@@ -13,7 +17,6 @@
 	<?php if ($title == "Estoque de Móveis - Painel de Controle"){
 		echo 
 	"<input type='checkbox' id='menu-toggle' />
-	<label for='menu-toggle' class='menu-icon'><i class='fa fa-bars'></i></label>
 	<div class='content-container'>";
 	} ?>
 	<div class="logo-container">
