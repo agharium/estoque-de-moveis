39390c36a8de35de6f69973f586262b701705511
Lucas Jardim <lcsjardim@gmail.com>
2017-11-29 14:35:25 -0200

    Imprimindo Ações de Produtos em Detalhes

--- app/views/home/produto/detalhes.php
+++ app/views/home/produto/detalhes.php
@@ -38,10 +38,49 @@
 					<td> <?php echo $dados->getQuantidade(); ?> </td>
 				</tr>
 			</table>
+
+			<div style="text-align: center">
+				<!-- Ações de Produtos -->
+
+				<a href="/estoque-de-moveis/produto/registro/<?php echo $dados->getCodigo().'-'.$dados->getQuantidade(); ?>-entrada" class="icone-entrada" title="Registrar entrada">
+					<!-- <i class="fa fa-sign-in"></i> -->
+					Registrar Entrada
+				</a>
+				&nbsp;|
+				<a href="/estoque-de-moveis/produto/registro/<?php echo $dados->getCodigo().'-'.$dados->getQuantidade(); ?>-saida" class="icone-saida" title="Registrar saída">
+					<!-- <i class="fa fa-sign-out"></i> -->
+					Registrar Saída
+				</a>
+				<?php if ($pm==2): ?>
+					&nbsp;|
+					<a href="/estoque-de-moveis/produto/editar/<?php echo PseudoCrypt::hash($dados->getCodigo()); ?>" class="icone-editar" title="Editar">
+						<!-- <i class='fa fa-pencil'></i> -->
+						Editar
+					</a>
+					&nbsp;|
+					<a onclick="confirmDialog(<?php echo $dados->getCodigo(); ?>)" style="cursor:pointer" class="icone-remover" title="Remover">
+						<!-- <i class='fa fa-trash-o'></i> -->
+						Remover
+					</a>
+				<?php endif; ?>
+
+				<!-- Fim das Ações -->
+			</div>
+
 		</div>
 	</div>
-
 </div>
+
+<!-- dialog pra excluir produto -->
+<script type="text/javascript">
+	function confirmDialog(codigo) {
+		if ( confirm("Voce deseja realmente apagar?") ) {
+			location.href = "/estoque-de-moveis/produto/remover/" + codigo;
+		}
+	}
+</script>
+<!-- fim do dialog -->
+
 <?php
 	require(VIEW_PATH . "/home/footer.php");
 ?>
