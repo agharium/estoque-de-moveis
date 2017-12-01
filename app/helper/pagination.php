57c0bf07654cc984c50110ec19e1f2fe77d8423b
José Paulo Oliveira Filho <oliveira.filho237@gmail.com>
2017-12-01 20:19:13 -0200

    pagination buttons + tooltips

--- app/helper/pagination.php
+++ app/helper/pagination.php
@@ -3,7 +3,7 @@
 	if ( !$pagBusca ) {
 		require_once("app/controllers/Produto.php");
 
-		$produtosPorPagina 			= 10;
+		$produtosPorPagina 					= 10;
 		$estoqueCount						= Produto::getEstoqueCount();
 
 		if ($estoqueCount >= 0){
@@ -11,7 +11,7 @@
 
 			echo "<div style='text-align: center;'>";
 			for ($i=1; $i<=$qtdPaginas; $i++) {
-				echo "<a href='/estoque-de-moveis/produto/page/$i'>" . $i . "</a> &nbsp;";
+				echo "<a class='button button-outline' href='/estoque-de-moveis/produto/page/$i'>" . $i . "</a> &nbsp;";
 			}
 			echo "</div>";
 		}
